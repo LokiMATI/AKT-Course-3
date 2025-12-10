20ace8533a6fab4f53c10074693370b4c54125c3
0109-13 <0109-13@0109-13>
2025-12-10 13:48:36 +0300

    Add Index

--- TRiZBD/LabWorks/LabWork17/WebApplication/Pages/Sessions/Index.cshtml.cs
+++ TRiZBD/LabWorks/LabWork17/WebApplication/Pages/Sessions/Index.cshtml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using WebApplication.Contexts;
 using WebApplication.Models;
 
@@ -21,11 +22,37 @@ namespace WebApplication.Pages.Sessions
 
         public IList<Session> Session { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? FilmTitle { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SortColumn { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Hall { get; set; }
+
         public async Task OnGetAsync()
         {
-            Session = await _context.Sessions
+            ViewData["Halls"] = new SelectList(_context.Halls, "HallId", "Информация");
+            var sessions = _context.Sessions
                 .Include(s => s.Film)
-                .Include(s => s.Hall).ToListAsync();
+                .Include(s => s.Hall)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(FilmTitle))
+                sessions = sessions.Where(s => s.Film.Title.Contains(FilmTitle));
+
+            sessions = SortColumn switch
+            {
+                "price" => sessions.OrderBy(s => s.Price),
+                "price_desc" => sessions.OrderByDescending(s => s.Price),
+                _ => sessions
+            };
+
+            if (!string.IsNullOrWhiteSpace(Hall) && int.TryParse(Hall, out int hallId))
+                sessions = sessions.Where(s => s.HallId == hallId);
+
+            Session = await sessions.ToListAsync();
         }
     }
 }
